c65e2a17e91e9032334373b9f58d8b9612f8d961
Jose Jaime Come <josejaimecome@gmail.com>
2023-01-17 10:50:25 +0200

    Calculation code updated

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,83 +1,124 @@
 import { useState, useEffect } from 'react'
 //import Button from './Button'
 
-
+let prevnmbr = '0';
 const Calculator = () => {
-  const [prevnmbr, nmbr] = useState(0)
-  return (
+  const [nmbr, setNmbr] = useState('');
+  const [lastop, setLastop] = useState('NA');  
+  let r = 0;
+  const addNmber = (e) => {
+    //console.log('L:' + lastop + ':');
+    if("=" === e.target.innerHTML){
+     if(lastop === "NA"){
+         return;
+     } else{
+         if(lastop === "-"){    
+            //console.log('' + prevnmbr + ',' + nmbr)        
+            r = Number(prevnmbr) - Number(nmbr);
+            setNmbr('' + r)
+            //console.log('r: ' + r);            
+         } else if(lastop === "+"){
+            r = Number(prevnmbr) + Number(nmbr);
+            setNmbr('' + r)
+         } else if(lastop === "/"){
+            r = Number(prevnmbr) / Number(nmbr);
+            setNmbr('' + r)
+         } else if(lastop === "x"){
+            r = Number(prevnmbr) * Number(nmbr);
+            setNmbr('' + r)
+         }
+     }
+     prevnmbr = '';
+     return;
+    } 
+    if (!("=/+-x").includes(e.target.innerHTML)){ 
+     setNmbr("" + nmbr + e.target.innerHTML);
+     console.log("N: " + nmbr)
+    } else {
+     prevnmbr = nmbr; 
+     setNmbr('');
+     setLastop(e.target.innerHTML);
+     console.log("O: " + prevnmbr + ',' + e.target.innerHTML)
+    }
+  };
+  const clean_screen = () => {
+     prevnmbr = '';
+     setNmbr('');
+     setLastop('NA');
+ }; 
+ return (
     <div>
          <table>
             <tr>
             <td colSpan="3">
                     <div id="lbl">
-                        <label></label>
+                        <label>{nmbr}</label>
                     </div>                    
                 </td>
                 <td>
-                    <button class="btn_clean" onClick="clean_screen()">C</button>
+                    <button class="btn_clean" onClick={clean_screen}>C</button>
                 </td>
             </tr>
             <tr>
                 <td>
-                    <button onClick="addNmber()">7</button>
+                    <button onClick={addNmber}>7</button>
                 </td>
                 <td>
-                    <button onClick="addNmber()">8</button>
+                    <button onClick={addNmber}>8</button>
                 </td>
                 <td>
-                    <button onClick="addNmber()">9</button>
+                    <button onClick={addNmber}>9</button>
                 </td>
                 <td>
-                    <button class="oper" onClick="addNmber()">/</button>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <button onClick="addNmber()">4</button>
-                </td>
-                <td>
-                    <button onClick="addNmber()">5</button>
-                </td>
-                <td>
-                    <button onClick="addNmber()">6</button>
-                </td>
-                <td>
-                    <button class="oper" onClick="addNmber()">x</button>
+                    <button class="oper" onClick={addNmber}>/</button>
                 </td>
             </tr>
             <tr>
                 <td>
-                    <button onClick="addNmber()">1</button>
+                    <button onClick={addNmber}>4</button>
                 </td>
                 <td>
-                    <button onClick="addNmber()">2</button>
+                    <button onClick={addNmber}>5</button>
                 </td>
                 <td>
-                    <button onClick="addNmber()">3</button>
+                    <button onClick={addNmber}>6</button>
                 </td>
                 <td>
-                    <button class="oper" onClick="addNmber()">-</button>
+                    <button class="oper" onClick={addNmber}>x</button>
                 </td>
             </tr>
             <tr>
                 <td>
-                    <button onClick="addNmber()">0</button>
+                    <button onClick={addNmber}>1</button>
                 </td>
                 <td>
-                    <button onClick="addNmber()">.</button>
+                    <button onClick={addNmber}>2</button>
                 </td>
                 <td>
-                    <button class="oper2" onClick="addNmber()">=</button>
+                    <button onClick={addNmber}>3</button>
                 </td>
                 <td>
-                    <button class="oper" onClick="addNmber()">+</button>
+                    <button class="oper" onClick={addNmber}>-</button>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <button onClick={addNmber}>0</button>
+                </td>
+                <td>
+                    <button onClick={addNmber}>.</button>
+                </td>
+                <td>
+                    <button class="oper2" onClick={addNmber}>=</button>
+                </td>
+                <td>
+                    <button class="oper" onClick={addNmber}>+</button>
                 </td>
             </tr>
          </table>
         
     </div>
-  )
+  ); 
 }
-
 
 export default Calculator
